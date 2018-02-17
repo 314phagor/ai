49de1cfd74641eb5eaf699c3e92eb8122658e04c
314phagor <36575672+314phagor@users.noreply.github.com>
2018-02-18 00:43:40 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
-
+emails = []
+jobs = []
+while True:
+  # Read unread emails into emails
+  # for i in emails:
+    # if i.subject[:18] == 'TradingView Alert:':
+      # command = i.text
+  if *keyboard_input: space* == True:
+    break
